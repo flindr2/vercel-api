743e07232464a25416bc277c4855d642eca9ade0
flindr2 <fredrik.lindroth@sprinto.se>
2024-09-16 06:41:35 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
-import express from 'express';
-import 'dotenv/config'
-import connect from './connect';
-import bookRoutes from './routes/bookRoutes';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connect from "./connect";
+import bookRoutes from "./routes/bookRoutes";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = 3000;
 
 connect();
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+  });
+});
+
 app.use("/api/", bookRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
+export default app;
